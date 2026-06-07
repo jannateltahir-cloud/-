06ce18142227879518ca47c4801a74b3407e7b6e
jannateltahir-cloud <jannateltahir@gmail.com>
2026-06-07 11:37:44 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "love you",
+        name: "Paradise angel",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#5c272e", 
+      secondary: "#bd5563", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
